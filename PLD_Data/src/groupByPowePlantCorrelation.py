e8d0c4a0f6e6d7507231708726588fb3dc73e77a
felipe <felipesantos956@poli.ufrj.br>
2019-01-28 00:23:08 -0200

    Added chapter 2 of dissertation

--- PLD_Data/src/groupByPowePlantCorrelation.py
+++ PLD_Data/src/groupByPowePlantCorrelation.py
@@ -27,57 +27,70 @@ matplotlib.rcParams['xtick.labelsize'] = 12
 matplotlib.rcParams['ytick.labelsize'] = 12
 matplotlib.rcParams['text.color'] = 'k'
 
-ROOT_FOLDER  = 'D:/Sistemas GIT/ElectricalEnergyPrice'
+ROOT_FOLDER  = '/home/felipe/Materias/TCC/'
 
 #loading PLD data
 MAIN_DIR = ROOT_FOLDER + '/PLD_Data/PLD_Outubro_2018'
 MAIN_DIR += '/10_out18_RV0_logENA_Mer_d_preco_m_0/'
 
 REGION = ['1 - Sudeste', '2 - Sul', '3 - Nordeste', '4 - Norte']
-regionFilter = REGION[1 - 1]
+REGION_ABBREVIATION = ['SE', 'S', 'NE', 'N']
+REGION_INDEX = 3
+regionFilter = REGION[REGION_INDEX - 1]
 
 INITIAL_YEAR = 2015
 INTIAL_DATE = '2015-01-01'
 FINAL_DATE = '2018-10-31'
 SAVE_CORR = True
-REGION_NAME = 'SE'
-COR_MATRIX_FIG_NAME = REGION_NAME + '_corVec_plot.jpg'
-COR_MATRIX_DATA_NAME = REGION_NAME + '_corVec.csv'
-PLOT_TITLE = 'Affluent Flow ' + REGION_NAME
-CORR_PLOT_TITLE = 'Correlation Between FLuviometric Stations - ' + REGION_NAME
-PLOT_FIG_NAME = REGION_NAME + '_affluentFlow.jpg'
+REGION_NAME = REGION_ABBREVIATION[REGION_INDEX - 1]
+#COR_MATRIX_FIG_NAME = REGION_NAME + '_corVec_plot.jpg'
+COR_MATRIX_FIG_NAME = 'SaoFrancisco_corVec_plot.jpg'
+#COR_MATRIX_DATA_NAME = REGION_NAME + '_corVec.csv'
+COR_MATRIX_DATA_NAME ='SaoFrancisco_corVec.csv'
+#PLOT_TITLE = 'Affluent Flow ' + REGION_NAME
+PLOT_TITLE = 'Affluent Flow São Francisco River' 
+#CORR_PLOT_TITLE = 'Correlation Between FLuviometric Stations - ' + REGION_NAME
+CORR_PLOT_TITLE = 'Correlation Between FLuviometric Stations - São Francisco River'
+#PLOT_FIG_NAME = REGION_NAME + '_affluentFlow.jpg'
+PLOT_FIG_NAME =  'SaoFrancisco_affluentFlow.jpg'
+FILTER_BY_FS = True
+#psID = [156, 158, 172, 173, 175, 176, 178, 300] #São Francisco
+psID = [191, 253, 257, 270, 271, 275] #Tocantins
 ##Settings
 
 #Extract all power stations in southeast region
 hidr = pd.read_csv(MAIN_DIR + 'HIDR.csv', sep=';')
-hidrSE = hidr.loc[hidr.Sistema == regionFilter]
-
+if not FILTER_BY_FS:
+    hidrFiltered = hidr.loc[hidr.Sistema == regionFilter]
+else:
+    hidrFiltered = hidr[hidr.Posto.isin(psID)]
+    
 affluentFlow = pd.read_csv(MAIN_DIR + 'VAZOES_DAT.TXT', sep='\s+', header=None)
 affluentFlow.columns = ['FS_ID', 'Year', 'Jan', 'Feb', 'Mar',\
                         'Apr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec']
 
 #Extract all affluent flow by fluviometric station in a range of time
-affluentFlowSE = pd.merge(affluentFlow, hidrSE, left_on='FS_ID', right_on='Posto')
-recentAFSE = affluentFlowSE.loc[affluentFlowSE.Year >= INITIAL_YEAR]
-recentAFSE = recentAFSE.iloc[:, 0:21]
-years = recentAFSE.Year.unique()
+affluentFlowSFiltered= pd.merge(affluentFlow, hidrFiltered, left_on='FS_ID', right_on='Posto')
+recentAFFiltered = affluentFlowSFiltered.loc[affluentFlowSFiltered.Year >= INITIAL_YEAR]
+recentAFFiltered = recentAFFiltered.iloc[:, 0:21]
+years = recentAFFiltered.Year.unique()
 numberOfYears = years.size
 
 #Transform data into temporal series
-AFSEseries = recentAFSE.loc[recentAFSE.Year == INITIAL_YEAR].iloc[:, 0:14]
-AFSEseries = AFSEseries.reset_index()
+AFFilteredseries = recentAFFiltered.loc[recentAFFiltered.Year == INITIAL_YEAR].iloc[:, 0:14]
+AFFilteredseries = AFFilteredseries.reset_index()
 for year in years:
     if year != INITIAL_YEAR:
-        tempAFSE = recentAFSE.loc[recentAFSE.Year == year].iloc[:, 2:14]
+        tempAFSE = recentAFFiltered.loc[recentAFFiltered.Year == year].iloc[:, 2:14]
         tempAFSE = tempAFSE.reset_index()
-        AFSEseries = pd.concat([AFSEseries, tempAFSE], axis=1)
+        AFFilteredseries = pd.concat([AFFilteredseries, tempAFSE], axis=1)
 
 #Get high correlated fluviometric station
-AFSEseries = AFSEseries.drop(columns=['Year', 'index'])
-AFSEseries = AFSEseries.iloc[:, :-2]
-AFSEseries.set_index('FS_ID', inplace=True)
-AFSEseriesT = AFSEseries.transpose()
-corr = AFSEseriesT.corr()
+AFFilteredseries = AFFilteredseries.drop(columns=['Year', 'index'])
+AFFilteredseries = AFFilteredseries.iloc[:, :-2]
+AFFilteredseries.set_index('FS_ID', inplace=True)
+AFFilteredseriesT = AFFilteredseries.transpose()
+corr = AFFilteredseriesT.corr()
 ax = plt.axes()
 sns.heatmap(corr, xticklabels=corr.columns, \
             yticklabels=corr.columns, cmap='Greens', ax=ax)
@@ -88,10 +101,10 @@ if SAVE_CORR:
     plt.savefig(COR_MATRIX_FIG_NAME, bbox_inches='tight')
     
 #Plot all affluent flow
-AFSESseriesPlot = AFSEseriesT
+AFFilteredSseriesPlot = AFFilteredseriesT
 months = util.GetMonthRange(INTIAL_DATE, FINAL_DATE)
-AFSESseriesPlot.index = months
-AFSESseriesPlot.plot(title=PLOT_TITLE, legend=False)
+AFFilteredSseriesPlot.index = months
+AFFilteredSseriesPlot.plot(title=PLOT_TITLE, legend=True)
 plt.ylabel('m\u00b3/s')
 plt.xlabel('Year')
 
@@ -100,7 +113,7 @@ if SAVE_CORR:
 
 #extend this to all fluviometric station
 #THRESHOLD = 2/sqrt(N) where N is the number of samples
-N = AFSEseries.columns.size
+N = AFFilteredseries.columns.size
 THRESHOLD = 2/np.sqrt(N)
 corrVec = corr.iloc[1, :]
 
@@ -116,4 +129,4 @@ if SAVE_CORR:
     corr.to_csv(COR_MATRIX_DATA_NAME, sep=";")
 
 #get total affluent flow
-FStation = AFSEseries.sum(axis = 1)
+FStation = AFFilteredseries.sum(axis = 1)
